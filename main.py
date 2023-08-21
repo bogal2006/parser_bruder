685350baf82c04b6d679c2818839e0b28ed93f0c
bogal2006 <51925338+bogal2006@users.noreply.github.com>
2023-08-21 10:01:50 +0500

    1_3

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import time
 from fake_useragent import UserAgent
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-
-
+import pandas as pd
 
 url = 'https://www.myfxbook.com/members/Nicholas54/bullbear-bot-lite/4392701'
 options = webdriver.ChromeOptions()
@@ -17,11 +16,16 @@ options.add_argument(f"user-agent={us.random}")
 options.add_argument('--disable-blink-features=AutomationControlled')
 
 driver = webdriver.Chrome(options=options)
+# result_dict = {'open_date': [], 'close_date': [], 'symbol': [], 'action': [], 'lots': [], 'open_price': [],
+#               'close_price'
+#             : [], 'pips': [], 'profit_usd': [], 'duration': [], 'gain': []}
+result_dict = dict()
+file_result = 'result.csv'
 
 try:
-    driver.maximize_window() # делаем большое окно браузера
-    driver.get(url=url) # загружаем страницу
-    time.sleep(4) # даем время загрузиться
+    driver.maximize_window()  # делаем большое окно браузера
+    driver.get(url=url)  # загружаем страницу
+    time.sleep(5)  # даем время загрузиться
     # соглашаемся со всеми куками
     driver.find_element(By.XPATH, '//*[@id="dismissGdprConsentBannerBtn"]').click()
     time.sleep(5)
@@ -30,31 +34,39 @@ try:
     # выжидаем время
     time.sleep(5)
     # жмем на всплывающее окно
-    driver.find_element(By.XPATH, '//*[@id="blockContinueLink"]').click()
-    time.sleep(5)
+    # driver.find_element(By.XPATH, '//*[@id="blockContinueLink"]').click()
+    # time.sleep(5)
     # выбираем вкладку history
     driver.find_element(By.NAME, 'history').click()
     time.sleep(5)
     # скрываем нижнее всплывающее окно
     driver.find_element(By.XPATH, '//*[@id="hideToolbarBtn"]/i').click()
     time.sleep(2)
+    i = 1
+    count = 0
     for i in range(1819):
-        requiredHtml = driver.page_source #  получаем объект html
+        requiredHtml = driver.page_source  # получаем объект html
         soup = BeautifulSoup(requiredHtml, 'lxml')
         string_one = soup.find_all('tr', class_='commentRow')
         for item in string_one:
             string_one_one = item.find_all('td')
-            date_1 = item.find_all(class_='brokerTime')
             for item_item in string_one_one:
-                print(item_item.text.strip())
+                i += 1
+                # print(i)
+                if i > 143:
+                    text_item = item_item.text.strip()
+                    if text_item is not None:
+                        count += 1
+                        result_dict [count] = [text_item]
+                # print(item_item.text.strip())
         # жмем на next
         driver.find_element(By.XPATH, '//*[@id="historyCont"]/div[2]/div/ul[1]/li[9]/a/i').click()
         time.sleep(1)
 
-
 except Exception as ex:
     print(ex)
 finally:
-    driver.close() #закрываем драйвер
+    driver.close()  # закрываем драйвер
     driver.quit()
-
+    df = pd.DataFrame(data=result_dict)
+    df.to_csv('result.csv')
